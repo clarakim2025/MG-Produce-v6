2d46e8f2dd4b2a144b21da80bdbc4bd4ddd615c7
clarakim2025 <clara@mgproduce.com>
2025-05-16 20:55:42 -0700

    Add files via upload

---
+++ productList.js
@@ -0,0 +1,15 @@
+const productList = [
+  { name: "Yellow Onion Jumbo 50lbs", price: "$12.00" },
+  { name: "Red Onion Jumbo 50lbs", price: "$13.50" },
+  { name: "White Onion Jumbo 50lbs", price: "$15.00" },
+  { name: "Napa Cabbage 50lbs", price: "$18.00" },
+  { name: "Green Onion Iceless 48ct", price: "$10.00" },
+  { name: "Fuji Apple (WA) 40lb", price: "$20.00" },
+  { name: "Gala Apple (CA) 40lb", price: "$21.00" },
+  { name: "Cilantro 60ct", price: "$9.00" },
+  { name: "Jalapeño Pepper 35lb", price: "$16.00" },
+  { name: "Zucchini 25lb", price: "$14.50" },
+  { name: "Korean Melon (Gift Box)", price: "$30.00" },
+  { name: "Vietnam Coconut 9ct", price: "$28.00" }
+];
+export default productList;
