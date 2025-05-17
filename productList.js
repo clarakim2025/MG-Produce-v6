cc9af9b554302e29ab986f8cf7ac6ee3dbb658ff
clarakim2025 <clara@mgproduce.com>
2025-05-17 05:14:57 -0700

    Add files via upload

--- productList.js
+++ productList.js
@@ -1,7 +1,6 @@
 const productList = [
-  { name: 'Apple' },
-  { name: 'Banana' },
-  { name: 'Cherry' },
+  { name: "Yellow Onion Jumbo 50lbs", price: "$12.00" },
+  { name: "Vietnam Coconut 9ct", price: "$28.00" },
+  { name: "Korean Melon Gift Box", price: "$30.00" }
 ];
-
 export default productList;
